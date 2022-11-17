ba41786a411607175b0b0f4714a24f07177f60e7
kiisi <kiisifelix06@gmail.com>
2022-11-17 22:35:33 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let nav_menu = document.querySelector(".nav-menubar span")
+let nav_close = document.querySelector(".mobile-nav-close--times")
+
+let mobile_nav = document.querySelector(".mobile-nav")
+
+nav_menu.addEventListener("click",()=>{
+    mobile_nav.classList.add("show")
+})
+nav_close.addEventListener("click",()=>{
+    mobile_nav.classList.remove("show")
+})
